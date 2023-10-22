cf5b6b74c809d6c42cc321512513288ad709db6a
Robert Sorete <soreterobert@gmail.com>
2023-10-22 17:27:31 +0300

    Transferat fisiere

---
+++ lab01/2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# a)
+def a():
+    # Semnal sinusoidal de 400Hz
+    # Frecventa de esantionare de 1600Hz
+
+    timp = np.arange(0, 4, 1/1600)
+    semnal = np.sin(2 * np.pi * 400 * timp)
+
+    plt.plot(timp, semnal)
+    plt.grid()
+    plt.show()
+
+def b():
+    # Un semnal sinusoidal de frecventa 800Hz care sa dureze 3 secunde
+
+    timp = np.arange(0, 3, 1/2400)
+    semnal = np.sin(2 * np.pi * 800 * timp)
+
+    plt.plot(timp, semnal)
+    plt.grid()
+    plt.show()
+
+def c():
+    # Un semnal de tip sawtooth de frecventa 240Hz
+
+if __name__ == '__main__':
+    # a()
+    # b()
+    c()
+
---
+++ lab01/notes.md
@@ -0,0 +1,12 @@
+# Lab 1
+
+f(t) - functie continua
+f[t] - discretizarea functiei 
+
+Esantionare = Sampling
+
+np.stem() - puncte </br>
+Esantioanele se afiseaza cu np.stem(), nu cu np.plot()
+
+
+
